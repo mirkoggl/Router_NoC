d47893e7bf4f047294e4a2d7b4fdf36e4a9773cc
mirkoggl <mirko.gagliardi@gmail.com>
2015-10-07 18:27:30 +0200

    commentato

--- net_input_interface.vhd
+++ net_input_interface.vhd
@@ -1,3 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Author: 	Mirko Gagliardi
+-- 
+-- Create Date:    01/10/2015
+-- Design Name: 
+-- Module Name:    Network Input Interface - rtl 
+-- Project Name:   Router_Mesh	
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 	 
+--
+-- Revision: v 0.3
+-- Additional Comments:
+--		Network Input Interface gestisce i dati in ingresso al Router su un dato canale. Ogni dato è bufferizzato in una Fifo circolare. 
+--		I dati in ingresso provengono dalla network Output Interface di un altro Router. Quando valid è alto, il componente salva il 
+--		dato in ingresso in coda alla Fifo ed alza il bit di ack ad indicare che la trasmissione è avvenuta con successo. Se la Fifo è 
+--		piena il dato non viene salvato e l'ack resta basso.
+--
+--			Output Interface				Input Interface
+--			________________				__________________
+--				       valid|-------------->|valid
+--					Data_Out|-------------->|Data_In
+--						ack	|<--------------|ack
+--							|				|
+--											
+--		Se la Fifo possiede almeno un dato, il bit empty è basso e la Control Unit analizza l'indirizzo del dato in testa per capire su quale
+--		interfaccia di uscita smistarlo. Quando il dato in testa è stato processato correttamente, la Control Unit asserisce il bit shft, ad 
+--		indicare che tale dato è stato processato correttamente e che quindi può essere estratto dalla Fifo. 
+--
+--
+--		     Input Interface				Control Unit
+--			________________				_________________	
+--					    shft|<--------------|shft
+--					   sdone|-------------->|sdone
+--					   empty|-------------->|empty
+--							|				|							
+--							|	            |
+--
+--		Quando la Fifo ha scartato il dato in testa alza il bit sdone ad indicare alla Control Unit che l'operazione è avvenuta con successo.
+--		I dati in uscita dalla Network Input Interface sono collegati all'ingresso della crossbar.
+--
+----------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -16,15 +62,15 @@ entity net_input_interface is
 		clk : in std_logic;
 		reset : in std_logic;
 		
-		Data_In : in std_logic_vector(DATA_WIDTH-1 downto 0);
-		valid   : in std_logic;
-		shft	  : in std_logic;
+		Data_In : in std_logic_vector(DATA_WIDTH-1 downto 0);	-- Data Input
+		valid   : in std_logic;								    -- Data Input valid
+		shft	: in std_logic;									-- Shift enable		
 		
-		sdone : out std_logic;
-		ack   : out std_logic;
-		full  : out std_logic;
-		empty : out std_logic;
-		Data_Out : out std_logic_vector(DATA_WIDTH-1 downto 0)
+		sdone : out std_logic;									-- Shift Done
+		ack   : out std_logic;									-- Data Input stored
+		full  : out std_logic;									-- Fifo Full
+		empty : out std_logic;									-- Fifo Empty
+		Data_Out : out std_logic_vector(DATA_WIDTH-1 downto 0)  -- Data Output, to the Crossbar Data Input
 	);
 end entity net_input_interface;
 
@@ -33,10 +79,7 @@ architecture RTL of net_input_interface is
 	constant max_vect : std_logic_vector(f_log2(FIFO_LENGTH) downto 0) := conv_std_logic_vector(FIFO_LENGTH, f_log2(FIFO_LENGTH)+1);
 	constant min_vect : std_logic_vector(f_log2(FIFO_LENGTH) downto 0) := (others => '0');
 	
-	type fifo_type is array (0 to FIFO_LENGTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
-	type state_type is (idle); 
-	
-	signal current_s : state_type; 
+	type fifo_type is array (0 to FIFO_LENGTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);	
 	
 	signal fifo_memory : fifo_type := (others => (others => '0'));
 	signal head_pt, tail_pt : std_logic_vector(f_log2(FIFO_LENGTH)-1 downto 0) := (others => '0');
@@ -60,7 +103,6 @@ begin
 	process (clk, reset)
 	begin
 		if reset = '1' then
-		  current_s <= idle;
 		  ack <= '0';
 		  sdone <= '0';
 		  head_pt <= (others => '0');
@@ -73,16 +115,16 @@ begin
 		  ack <= '0';
 		  sdone <= '0';
 		      
-		  if valid ='1' then
-			  if fifo_full = '1' then
+		  if valid ='1' then		    -- Data input valid
+			  if fifo_full = '1' then	-- Data input cannot be stored
 				 ack <= '0';
 			  else
-				 fifo_memory(conv_integer(tail_pt)) <= Data_In; 
+				 fifo_memory(conv_integer(tail_pt)) <= Data_In; 	-- Data input stored correctly
 				 ack <= '1';
 				 tail_pt <= tail_pt + '1';
 				 elem_count <= elem_count + '1';
 		      end if;
-		  elsif shft = '1' then
+		  elsif shft = '1' then			-- Top Fifo data eliminated
 			  if fifo_empty = '0' then
 				 elem_count <= elem_count - '1';
 				 head_pt <= head_pt + '1';
--- net_output_interface.vhd
+++ net_output_interface.vhd
@@ -1,3 +1,53 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Author: 	Mirko Gagliardi
+-- 
+-- Create Date:    01/10/2015
+-- Design Name: 
+-- Module Name:    Network Output Interface - rtl 
+-- Project Name:   Router_Mesh	
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 	 
+--
+-- Revision: v 0.3
+-- Additional Comments:
+--		Network Output Interface gestisce i dati in uscita dal Router su un dato canale. Ogni dato è bufferizzato in una Fifo circolare. 
+--		Quando la Fifo non è vuota tenta di inviare il dato in testa all'Input Interface Network del Router con cui è collegato. L'invio del
+--		dato e l'attesa dell'ack sono gestiti con una FSM a due stati.
+--
+--			Output Interface				Input Interface
+--			________________				__________________
+--				       valid|-------------->|valid
+--					Data_Out|-------------->|Data_In
+--						ack	|<--------------|ack
+--							|				|
+--											
+--		Quando il dato è pronto l'unità di controllo asserisce valid e si pone in attesa dell'ack da parte del Router ricevente. La
+--		FSM resterà in attesa dell'ack per un numero di cicli pari a quelli indicati nella verabiale COUNTER_WIDTH nel routerpack.
+--
+--
+--		    Output Interface				Control Unit
+--			________________				_________________	
+--					    wren|<--------------|wren
+--					   sdone|-------------->|sdone
+--						full|-------------->|full
+--							|				|________________							
+--							|					
+--							|				Crossbar
+--							|				_________________
+--					 Data_In|<--------------|Data_Out
+--							|				|
+--
+--		Network Output Interface riceve i dati da inviare dalla Crossbar che collega tutti gli Network Input Interface del router a tutti
+--		gli Network Output Interface. Quando il dato in ingresso è valido, la Control Unit asserisce wren. Se la FIFO non è piena, il dato
+--		in ingresso è aggiunto in coda e sdone è asserito ad indicare che il salvataggio è stato effettuato correttamente. Se la Fifo è piena
+--		full è alto e la Control Unit agirà di conseguenza.
+--
+----------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -17,22 +67,22 @@ entity net_output_interface is
 		clk : in std_logic;
 		reset : in std_logic;
 		
-		Data_In : in std_logic_vector(DATA_WIDTH-1 downto 0);
-		ack   : in std_logic;
-		wren  : in std_logic;
+		Data_In : in std_logic_vector(DATA_WIDTH-1 downto 0);   -- Data Input
+		ack   : in std_logic;									-- Ack 
+		wren  : in std_logic;									-- Write Enable
 		
-		sdone : out std_logic;
-		full  : out std_logic;
-		empty : out std_logic;
-		valid : out std_logic;
-		Data_Out : out std_logic_vector(DATA_WIDTH-1 downto 0)
+		sdone : out std_logic;									-- Store Done
+		full  : out std_logic;									-- Fifo Full
+		empty : out std_logic;									-- Fifo Empty
+		valid : out std_logic;									-- Data Output valid
+		Data_Out : out std_logic_vector(DATA_WIDTH-1 downto 0)  -- Data Output
 	);
 end entity net_output_interface;
 
 architecture RTL of net_output_interface is
 	
-	constant max_vect : std_logic_vector(f_log2(FIFO_LENGTH) downto 0) := conv_std_logic_vector(FIFO_LENGTH, f_log2(FIFO_LENGTH)+1);
-	constant min_vect : std_logic_vector(f_log2(FIFO_LENGTH) downto 0) := (others => '0');
+	constant MAX_VECT : std_logic_vector(f_log2(FIFO_LENGTH) downto 0) := conv_std_logic_vector(FIFO_LENGTH, f_log2(FIFO_LENGTH)+1);
+	constant MIN_VECT : std_logic_vector(f_log2(FIFO_LENGTH) downto 0) := (others => '0');
 	constant MIN_COUNT : std_logic_vector(f_log2(COUNTER_WIDTH)-1 downto 0) := (others => '0');
 	constant MAX_COUNT : std_logic_vector(f_log2(COUNTER_WIDTH)-1 downto 0) := (others => '1');
 	
@@ -51,10 +101,10 @@ architecture RTL of net_output_interface is
 	
 begin
 	
-	fifo_full <= '1' when elem_count = max_vect
+	fifo_full <= '1' when elem_count = MAX_VECT
 						else '0';
 	
-	fifo_empty <= '1' when elem_count = min_vect		
+	fifo_empty <= '1' when elem_count = MIN_VECT		
 						else '0'; 
 	
 	full <= fifo_full;
@@ -62,7 +112,7 @@ begin
 	Data_Out <= fifo_memory(conv_integer(head_pt));
 	
 
-	process (clk, reset)
+	Output_Interface_Control_Unit : process (clk, reset)
 	begin
 		if reset = '1' then
 		  current_s <= idle;
@@ -80,16 +130,16 @@ begin
 		  
 		  case current_s is
 		     when idle =>       
-			     if wren ='1' and fifo_full = '0' then
+			     if wren = '1' and fifo_full = '0' then		-- Store data input
 			      	fifo_memory(conv_integer(tail_pt)) <= Data_In; 
 			      	sdone <= '1';
 			      	tail_pt <= tail_pt + '1';
 			      	elem_count <= elem_count + '1';
 			      	current_s <= idle;
-			    elsif fifo_empty = '0' then
+			    elsif fifo_empty = '0' then					-- Send Fifo first element
 			    	valid <= '1';
 					current_s <= wait_ack;
-					ack_counter <= MIN_COUNT;
+					ack_counter <= MIN_COUNT;				-- Set Ack counter
 				 else
 					current_s <= idle;
 			     end if;   
@@ -100,7 +150,7 @@ begin
 					elem_count <= elem_count - '1';
 					head_pt <= head_pt + '1';
 					current_s <= idle;
-				elsif ack_counter = MAX_COUNT then
+				elsif ack_counter = MAX_COUNT then			-- Stop waiting ack and back idle
 					current_s <= idle;
 					valid <= '0';
 				else
